75a273ada92b5be390baa8f6364e91c26ed16542
Muhammad Rehan <rehanch642@gmail.com>
2022-10-04 22:58:39 +0500

    feat: create prive converter library

--- FundMe.sol
+++ FundMe.sol
@@ -1,20 +1,16 @@
-pragma solidity ^"0.8.0";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "./PriceConverter.sol";
 contract FundMe{
-  uint256 public minimumUsd = 50;
-  require(msg.value >= minimumUsd, "Didn't send enough");
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 50 * 1e18;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
-  function getPrice() public view returns(uint256){
-    // address  0xA39434A63A52E749F02807ae27335515BA4b07F7
-    AggregatorV3Interface priceFeed = AggregatorV3Interface(0xA39434A63A52E749F02807ae27335515BA4b07F7)
-    (, int256 price, , , ) = priceFeed.latestRoudData();
-    return unint256(price * 1e10);
-  }
-
-  function getConversionRate(uint256 ethAmount) public view returns(uint256){
-    uint256 ethPrice = getPrice();
-    uint256n ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-    return ethAmountInUsd;
-  }
+    function fund() public payable{
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
 
 }
---
+++ PriceConverter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+    function getPrice() internal view returns(uint256){
+        // address  0xA39434A63A52E749F02807ae27335515BA4b07F7
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xA39434A63A52E749F02807ae27335515BA4b07F7);
+        (,int256 price,,,) = priceFeed.latestRoudData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
